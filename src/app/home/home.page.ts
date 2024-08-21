72df086b476bf2065a6bc61f4450ce20fc0c65ef
tanakorn ouaiheng <tanakhoniop@gmail.com>
2024-08-21 14:16:49 +0700

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private toastController: ToastController) {}
+
+  async presentToast(position:'middle') {
+    const toast = await this.toastController.create({
+      message: 'ฮั่นแน่!! เป็นเจ้าของร้านออเราอ่ะ',
+      duration: 1500,
+      position: position,
+    });
+
+    await toast.present();
+  }
+
+}
