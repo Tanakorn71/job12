bf70582e1f22b4c84a02c19b0c7b6b31ca48d3a1
tanakorn ouaiheng <tanakhoniop@gmail.com>
2024-08-27 13:03:44 +0700

    27/8/67

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { AboutPage } from './about/about.page';
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private a: NavController) {}
+
+  Aboutpage(){
+    this.a.navigateForward('/about')
+  }
+
 }
